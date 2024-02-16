1a146e1c22669dfa539815b0082928f3cd5fce64
James Slaughter <jmslaughter85@outlook.com>
2024-02-15 20:29:26 -0600

    better responsive layout for home page

--- pages/index.js
+++ pages/index.js
@@ -21,82 +21,61 @@ const LINK_ITEM = {
 */
 
 export default function Home() {
-  return (
-    <Layout>
-      <main className={styles.main}>
-        <Image
-          src="/AUG-logo-transparent-background-1.png"
-          width={365}
-          height={183}
-          alt="Antioch Urban Growers"
-        />
-        <div className="article-content">
-          <div
-            style={{
-              height: "1px",
-              width: "30rem",
-              backgroundColor: "black",
-              margin: "auto",
-            }}
-          />
-          <ul style={{ margin: "auto" }}>
-            <li style={{ paddingBottom: "2rem" }}>
-              <a
-                href="https://antiochurbang.square.site"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Antioch Urban Growers Storefront
-              </a>
-            </li>
-            <li
-              style={{
-                fontWeight: "bold",
-                paddingTop: 0,
-              }}
-            >
-              Follow us on Facebook for events, videos, and education!
-            </li>
-            <li>
-              <a
-                href="https://www.facebook.com/antiochurbangrowers"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  src="/fb.svg"
-                  width={72}
-                  height={72}
-                  alt="Our Facebook Page"
-                />
-              </a>
-            </li>
-            <li>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3093.04905139692!2d-94.5500099!3d39.1736056!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0f9c3def32867%3A0xf72ad06e93249453!2sAntioch%20Urban%20Growers!5e0!3m2!1sen!2sus!4v1668881845120!5m2!1sen!2sus"
-                width="600"
-                height="450"
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.google.com/maps/place/Antioch+Urban+Growers/@39.1736056,-94.5500099,17z/data=!4m13!1m7!3m6!1s0x87c0f9c3c2452193:0xcff71674a50bec0a!2s2727+NE+44th+St,+Kansas+City,+MO+64117!3b1!8m2!3d39.1735968!4d-94.5478165!3m4!1s0x87c0f9c3def32867:0xf72ad06e93249453!8m2!3d39.1736056!4d-94.5478212"
-              >
-                2727 NE 44th St, Kansas City, MO 64117
-              </a>
-            </li>
-            <li>
-              <a href="tel:+18166884953">(816) 699-4953</a>
-            </li>
-          </ul>
-        </div>
-        {/*
+	return (
+		<Layout>
+			<main className={styles.main}>
+				<Image
+					src="/AUG-logo-transparent-background-1.png"
+					width={365}
+					height={183}
+					alt="Antioch Urban Growers"
+				/>
+				<div style={{ paddingBottom: "2rem", fontSize: "3rem" }}>
+					<a
+						href="https://antiochurbang.square.site"
+						target="_blank"
+						rel="noreferrer"
+					>
+						Store
+					</a>
+				</div>
+				<div>
+					<a
+						target="_blank"
+						rel="noreferrer"
+						href="https://www.google.com/maps/place/Antioch+Urban+Growers/@39.1736056,-94.5500099,17z/data=!4m13!1m7!3m6!1s0x87c0f9c3c2452193:0xcff71674a50bec0a!2s2727+NE+44th+St,+Kansas+City,+MO+64117!3b1!8m2!3d39.1735968!4d-94.5478165!3m4!1s0x87c0f9c3def32867:0xf72ad06e93249453!8m2!3d39.1736056!4d-94.5478212"
+					>
+						2727 NE 44th St, Kansas City, MO 64117
+					</a>
+				</div>
+				<div
+					style={{
+						fontWeight: "bold",
+						paddingTop: 0,
+            textAlign: "center",
+            lineHeight: 1.5
+					}}
+				>
+					Follow us on Facebook for events, videos, and education!
+				</div>
+				<div>
+					<a
+						href="https://www.facebook.com/antiochurbangrowers"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<Image
+							src="/fb.svg"
+							width={72}
+							height={72}
+							alt="Our Facebook Page"
+						/>
+					</a>
+				</div>
+				<div>
+					<a href="tel:+18166884953">(816) 699-4953</a>
+				</div>
+				{/*
           <div style={LINK_CONTAINER}>
           <div style={LINK_ITEM}>Community Farms</div>
           <div style={LINK_ITEM}>Join our Discord</div>
@@ -104,7 +83,7 @@ export default function Home() {
           <div style={LINK_ITEM}>Our Store</div>
           </div>
         */}
-      </main>
-    </Layout>
-  );
+			</main>
+		</Layout>
+	);
 }
